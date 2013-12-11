769a761c5b88b9d2d470ed88a60b9c4c5fb5e189
rusma <info@pimmeijer.com>
2013-12-11 16:46:01 +0100

    working calculation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,22 +4,109 @@
 
 angular.module('film_factor.controllers', []).
     controller('film_factor_controller', function($scope, maiVCApiService, localStorageService) {
-        $scope.loadGenre = function() {
-            var dfrd = $.Deferred();
+        $scope.getGenreData = function() {
+            // var dfrd = $.Deferred();
 
-            console.log(localStorageService.get('topscorersPL'));
-            if(localStorageService.get('localStorageKey') !== null) {
-                maiVCApiService.getGenreData().success(function(response){
-                    if(response.length != 0 || response.error != null) {
-                        localStorageService.add('genreData',response);
-                        dfrd.resolve(response);
-                    }
+            // console.log(localStorageService.get('topscorersPL'));
+            // if(localStorageService.get('localStorageKey') !== null) {
+            //     maiVCApiService.getGenreData().success(function(response){
+            //         if(response.length != 0 || response.error != null) {
+            //             localStorageService.add('genreData',response);
+            //             dfrd.resolve(response);
+            //         }
+            //     });
+            // } else {
+            //     var response = localStorageService.get('genreData');
+            //     dfrd.resolve(response);
+            // }
+
+            // return dfrd.promise();
+
+            var data = [],
+               shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
+               groups = 22;
+
+                 
+            for (var i = 0; i <= groups; i++) {
+                data.push({
+                   key: 'Genre ' + i,
+                   values: []
                 });
-            } else {
-                var response = localStorageService.get('genreData');
-                dfrd.resolve(response);
+
+                var coordinates = $scope.getXandYForRatingAndFactor(i, Math.random(0,100), groups);
+                for (var j = 0; j < 50; j++) {
+                    
+                    data[i].values.push({
+                        x: coordinates.x,
+                        y: coordinates.y,
+                        size: Math.random()
+                   //, shape: shapes[j % 6]
+                    });
+                }
             }
 
-            return dfrd.promise();
-        }
+            // data.push({
+            //        key: 'Genre 0',
+            //        values: []
+            //     });
+
+            // var coordinates = $scope.getXandYForRatingAndFactor(6, 20, groups);
+            // data[0].values.push({
+            //             x: coordinates.x,
+            //             y: coordinates.y,
+            //             size: 30
+            //        //, shape: shapes[j % 6]
+            //         });
+
+
+            //    data.push({
+            //        key: 'Genre 1',
+            //        values: []
+            //     });
+
+            // var coordinates = $scope.getXandYForRatingAndFactor(11, 90, groups);
+            // data[1].values.push({
+            //             x: coordinates.x,
+            //             y: coordinates.y,
+            //             size: 10
+            //        //, shape: shapes[j % 6]
+            //         });
+
+            //      data.push({
+            //        key: 'Genre 2',
+            //        values: []
+            //     });
+
+            // var coordinates = $scope.getXandYForRatingAndFactor(18, 90, groups);
+            // data[2].values.push({
+            //             x: coordinates.x,
+            //             y: coordinates.y,
+            //             size: 20
+            //        //, shape: shapes[j % 6]
+            //         });
+            // console.log(data);
+            return data;
+        };
+
+        $scope.getXandYForRatingAndFactor = function(group_index, rating, group_length) {
+            console.log(rating);
+
+            var degrees_for_group = 360 / group_length;
+             //16.363636363636363 controllers.js:67
+
+            console.log('d f group ' + degrees_for_group)
+
+            var location = degrees_for_group * group_index;
+            console.log('location ' + location )
+            //location 98.18181818181819 
+            var pi = Math.PI;
+            var x = Math.sin(location * pi / 180) * ((rating -100) * -1);
+            console.log(x)
+
+            var y = Math.cos(location * pi / 180) * ((rating -100) * -1);
+            console.log(x, y);
+            return {x: x, y: y};
+           
+        };
+
    	});
--- app/js/directives.js
+++ app/js/directives.js
@@ -7,74 +7,50 @@ angular.module('film_factor.directives', []).
   directive('bubbleChart', function() {
     return {
             restrict: 'A', // Directive Scope is Attribute
-            scope: {
-                datajson: '=',
-                xaxisName: '=',
-                xaxisPos: '=',
-                yaxisName: '=',
-                yaxisPos: '=',
-                d3Format: '=',
-                // All the Angular Directive Vaiables used as d3.js parameters
-            },
             link: function (scope, elem, attrs) {
                 // The d3.js code for generation of bar graph. further reading should be done from http://d3js.org/
                 console.log(elem);
                 nv.addGraph(function() {
                     var index = 2;
-                    var x = 200;
-                    var y = 10;
                     var chart = nv.models.scatterChart()
-                                .showXAxis(false)
-                                .showYAxis(false)
+                                .width(600)
+                                .height(600)
+                                .showXAxis(true)
+                                .showYAxis(true)
+                                .forceX([-100,100])
+                                .forceY([-100,100])
                                 .tooltips(true)
-                                .width(1000)
-                                .height(500)
+                                .showLegend(false)
                                 .tooltipContent(function(){
-                                    return 'cock';
-                                })
-                                .color(d3.scale.category10().range());
+                                    return 'clock';
+                                });
+                                //.color(d3.scale.category10().range());
 
+                    var tickMarks = [0, -50, 50, 100];
+
+                    chart.xAxis
+                        .tickValues(tickMarks)
+                        .tickFormat(function(d){ return d });
+
+                            chart.yAxis
+                        .tickValues(tickMarks)
+                        .tickFormat(function(d){ return d });
 
                     // animation example
                     d3.select(elem[0])
-                        .datum(aap(2,40))
+                        .datum(scope.getGenreData())
                           .transition().duration(1200)
                     .call(chart);
 
-                     d3.select(elem[0])
-                        .datum(aap(2,20))
-                          .transition().duration(1200)
-                    .call(chart);
+                    //  d3.select(elem[0])
+                    //     .datum(aap(2,20))
+                    //       .transition().duration(1200)
+                    // .call(chart);
 
                     nv.utils.windowResize(chart.update);    
 
                     return chart; 
                 }); 
-
-                var aap = function randomData(groups, points) { //# groups,# points per group
-                   var data = [],
-                       shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
-                       random = d3.random.normal();
-                 
-                    for (var i = 0; i < groups; i++) {
-                         data.push({
-                           key: 'Group ' + i,
-                           values: []
-                         });
-                     
-                    for (var j = 0; j < points; j++) {
-                       data[i].values.push({
-                         x: random()
-                       , y: random()
-                       , size: Math.random()
-                       //, shape: shapes[j % 6]
-                       });
-                     }
-                   }
-             
-               return data;
-             }
-               
-           }
+            }
        }
     });
