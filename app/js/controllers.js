6955d45341041b806c6ac29f5b5f0cf460081ef8
Pim Meijer <info@pimmeijer.com>
2013-12-16 16:13:06 +0100

    little fix for groups

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,21 +5,28 @@
 angular.module('film_factor.controllers', []).
     controller('film_factor_controller', function($scope, maiVCApiService, localStorageService) {
         $scope.genres = {
-            1: "erotic",
-            2: "horror",
-            4: "romance",
-            6: "comedy",
-            7: "drama",
-            8: "kids-family",
-            9: "science-fiction-fantasy",
-            10: "fantasy",
-            11: "action-adventure",
-            12: "action",
-            13: "crime",
-            14: "family",
-            16: "thriller",
-            17: "adventure",
-            18: "science-fiction"
+            0: {"genre_id": 1, "genre": "erotic"},
+            1: {"genre_id": 2, "genre": "horror"},
+            2: {"genre_id": 4, "genre": "romance"},
+            3: {"genre_id": 6, "genre": "comedy"},
+            4: {"genre_id": 7, "genre": "drama"},
+            5: {"genre_id": 8, "genre": "kids-family"},
+            6: {"genre_id": 9, "genre": "romance"},
+            7: {"genre_id": 10, "genre": "fantasy"},
+            8: {"genre_id": 11, "genre": "action-adventure"},
+            9: {"genre_id": 12, "genre": "action"},
+            10: {"genre_id": 13, "genre": "crime"},
+            11: {"genre_id": 14, "genre": "family"},
+            12: {"genre_id": 16, "genre": "thriller"},
+            13: {"genre_id": 17, "genre": "adventure"},
+            14: {"genre_id": 18, "genre": "science-fiction"},
+            15: {"genre_id": 19, "genre": "animation"},
+            16: {"genre_id": 20, "genre": "documentary"},
+            17: {"genre_id": 21, "genre": "music"},
+            18: {"genre_id": 25, "genre": "western"},
+            19: {"genre_id": 26, "genre": "sports-film"},
+            20: {"genre_id": 27, "genre": "war"},
+            21: {"genre_id": 32, "genre": "eastern"}
         };
 
         $scope.getGenreMovies = function() {
@@ -47,20 +54,31 @@ angular.module('film_factor.controllers', []).
             var genres = $scope.genres;
 
             //only works in modern browsers
-            var group_length = ( Object.keys(genres).length ) ? Object.keys(genres).length : 15;
+            var group_length = ( Object.keys(genres).length ) ? Object.keys(genres).length : 22;
 
             //create array index for each genre in movies array
             //this will contain our filtered data with x and y etc
-            _.each(genres, function(val, key){
-                movies.push({
-                   id: key,
-                   key: 'Genre ' + val,
+            var test = 0;
 
-                   values: []
+                _.each(genres, function(val, key){
+
+                    if(test < 22) {
+
+                    movies.push({
+                        id: key,
+                        key: val.genre,
+                        data: val,
+                        values: []
+                    });
+                    test++;
+                    } else {
+                        return false;
+                    }
                 });
-            });
 
-            //giant loop structur
+
+
+            //giant loop structure - performance intensive - figure something else for this
             _.each(unfiltered_movie_data, function(unfiltered_movie_data_val, unfiltered_movie_key){
 
                 //check if the unfiltered data has genres
@@ -72,20 +90,23 @@ angular.module('film_factor.controllers', []).
                         if(unfiltered_movie_data_val.rating_tmdb_audience === 0 ||
                             unfiltered_movie_data_val.rating_tmdb_audience === '') {
                             return false;
+                        } else {
+                            //if there is add a value to the genre in which the unfiltered movie belongs
+                            _.each(unfiltered_movie_data_val.genres, function(val, index){
+                                // console.log(val, movie_val.key.genre_id);
+                                if(val === parseInt(movie_val.data.genre_id)) {
+                                    var coordinates = $scope.getXandYForRatingAndFactor(movie_val.id, unfiltered_movie_data_val.rating_tmdb_audience, group_length);
+                                    movie_val.values.push({
+                                        x: coordinates.x,
+                                        y: coordinates.y,
+                                        size: 1,
+                                        title: unfiltered_movie_data_val.title,
+                                        rating: unfiltered_movie_data_val.rating_tmdb_audience,
+                                        genre: movie_val.data.genre
+                                    });
+                                }
+                            });
                         }
-                        //if there is add a value to the genre in which the unfiltered movie belongs
-                        _.each(unfiltered_movie_data_val.genres, function(val, index){
-                            if(val === parseInt(movie_val.id)) {
-                                var coordinates = $scope.getXandYForRatingAndFactor(movie_val.id, unfiltered_movie_data_val.rating_tmdb_audience, group_length);
-                                movie_val.values.push({
-                                    x: coordinates.x,
-                                    y: coordinates.y,
-                                    size: 1,
-                                    title: unfiltered_movie_data_val.title,
-                                    rating: unfiltered_movie_data_val.rating_tmdb_audience,
-                                });
-                            }
-                        });
                     });
                 }
             });
@@ -102,15 +123,14 @@ angular.module('film_factor.controllers', []).
               _.each(genres, function(val, key){
                     movies.push({
                        id: key,
-                       key: 'Genre ' + val,
-
+                       key: val.genre,
                        values: []
                     });
                 });
 
                 for(var i in movies) {
-                    for (var j = 0; j < 50; j++) {
-                        var coordinates = $scope.getXandYForRatingAndFactor(i, Math.random(0,100), group_length);
+                    for (var j = 0; j < 20; j++) {
+                        var coordinates = $scope.getXandYForRatingAndFactor(i, Math.floor(Math.random() * 100) + 1, group_length);
                         movies[i].values.push({
                             x: coordinates.x,
                             y: coordinates.y,
@@ -124,6 +144,9 @@ angular.module('film_factor.controllers', []).
             },
 
         $scope.getXandYForRatingAndFactor = function(group_index, rating, group_length) {
+            console.log(group_index, rating, group_length);
+
+            // console.log(group_index, rating, group_length);
             var degrees_for_group = 360 / group_length;
              //16.363636363636363 controllers.js:67
 
@@ -131,7 +154,8 @@ angular.module('film_factor.controllers', []).
             var end_of_group = Math.random() * (degrees_for_group * 2);
 
             // console.log(degrees_for_group, end_of_group);
-            var location = (Math.floor( end_of_group ) + ( degrees_for_group ) ) * group_index;
+            var location =  (Math.random() * end_of_group) + degrees_for_group * group_index;
+            //var location = (Math.floor( end_of_group ) + ( degrees_for_group ) ) * group_index;
             // console.log('location ' + location )
             //location 98.18181818181819
             var pi = Math.PI;
--- app/js/directives.js
+++ app/js/directives.js
@@ -18,38 +18,32 @@ angular.module('film_factor.directives', []).
                                 .forceX([-100,100])
                                 .forceY([-100,100])
                                 .tooltips(true)
-                                .color(d3.scale.category10().range())
                                 .showLegend(true)
+                                .color(﻿["CD4A4A", "FD5E53","EA7E5D","FF8243","D68A59","FFA343","8A795D","FCE883","FDFC74","1DF914","1CAC78","17806D","80DAEB","9ACEEB","C5D0E6","ADADD6","CDA4DE","FF1DCE","6E5160","F75394","DE5D83","EE204D"])
                                 .tooltipContent(function(key, y, e, graph){
-                                    return graph.point.title + " <br> Rating: " + graph.point.rating;
+                                    return graph.point.title + " <br> Rating: " + graph.point.rating + "<br> " + graph.point.genre;
                                 });
+                    console.log(d3.scale.category20().range());
+                    var tickMarks = [0, -50, 50, 100];
 
-                    // var tickMarks = [0, -50, 50, 100];
-
-                    // chart.xAxis
-                    //     .tickValues(tickMarks)
-                    //     .tickFormat(function(d){ return d });
-                    // chart.yAxis
-                    //     .tickValues(tickMarks)
-                    //     .tickFormat(function(d){ return d });
+                    chart.xAxis
+                        .tickValues(tickMarks)
+                        .tickFormat(function(d){ return d });
+                    chart.yAxis
+                        .tickValues(tickMarks)
+                        .tickFormat(function(d){ return d });
 
                     d3.select(elem[0])
                         .datum(scope.generateRandomGenreMoviesData())
-                          .transition().duration(1000)
+                          .transition().duration(1500)
                     .call(chart);
 
                     scope.getGenreMovies().then(function(data){
                         d3.select(elem[0])
                             .datum(data)
-                              .transition().duration(2000)
+                              .transition().duration(1000)
                         .call(chart);
                     });
-
-
-                    //  d3.select(elem[0])
-                    //     .datum(aap(2,20))
-                    //       .transition().duration(1200)
-                    // .call(chart);
 
                     nv.utils.windowResize(chart.update);
 
