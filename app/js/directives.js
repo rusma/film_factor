69faef2505903f638d0e556094fea31352b6887d
Robin Floris <rfloris92@gmail.com>
2013-12-18 14:36:30 +0100

    Changed the legend sequence and position

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,15 +8,14 @@ angular.module('film_factor.directives', []).
     return {
             restrict: 'A', // Directive Scope is Attribute
             link: function (scope, elem, attrs) {
-                    var _scope = scope
-                    var _elem = elem;
-                    
-                    scope.initChart().then(function(){
-                        scope.getGenreMovies().then(function(data){
-                            _scope.renderChart(_elem[0], data);
-                        });
+                var _scope = scope
+                var _elem = elem;
+                
+                scope.initChart().then(function(){
+                    scope.getGenreMovies().then(function(data){
+                        _scope.renderChart(_elem[0], data);
                     });
-
+                });
             },
        }
     });
