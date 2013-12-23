54f7f4e1b27f930442a8f4531c91d71de5928d9a
Pim Meijer <info@pimmeijer.com>
2013-12-23 13:02:25 +0100

    alter legend function refactor

--- app/js/controllers/main_controller.js
+++ app/js/controllers/main_controller.js
@@ -6,10 +6,10 @@
 
 angular.module('film_factor.controllers', []).
     controller('main_controller', function($scope, maiVCApiService, localStorageService) {
-        // The d3.js code for generation of bar graph. further reading should be done from http://d3js.org/
         $scope.chart = null;
 
         $scope.initChart = function() {
+            //use deffered to deal with async
             var dfrd = $.Deferred();
 
             nv.addGraph(function() {
@@ -26,16 +26,14 @@ angular.module('film_factor.controllers', []).
                                 "FCE883","FDFC74","1DF914","1CAC78","17806D","80DAEB","9ACEEB",
                                 "C5D0E6","ADADD6","CDA4DE","FF1DCE","6E5160","F75394","DE5D83","EE204D"])
                             .tooltipContent(function(key, y, e, graph){
-                                return graph.point.title + 
-                                " <br> Rating: " + graph.point.rating + 
+                                return graph.point.title +
+                                " <br> Rating: " + graph.point.rating +
                                 "<br> " + graph.point.genre;
-                                
-                          });
-                          
-                 
-                          
+                            });
+
                  nv.utils.windowResize($scope.chart.update);
 
+                 //deffered resolve
                  dfrd.resolve();
             });
 
@@ -47,24 +45,30 @@ angular.module('film_factor.controllers', []).
                 .datum(data)
                   .transition().duration(800)
             .call($scope.chart);
-            for(var i = 0; i<16; i++){
-	                 var translateX = 0;
-	                 var translateY = 18 * i;
-	                 
-	                 if(i > 8){
-		                var newY = i;
-		                newY -= 7;
-		                translateY = newY * 18;
-		                translateX = 75;
-	                 }
-	                 
-	                 console.log(translateX)
-	                 console.log(translateY)
-	                 console.log(d3.select(".nv-series:nth-child(1)"));
-	                 d3.select(".nv-series:nth-child("+i+")")
-					 	.attr("transform", "translate("+translateX+","+translateY+")");
-                 }     
-			
+
+            $scope.transformLegend;
         };
 
-   	});
+        //TO DO: add comments to this fucntion
+        $scope.transformLegend = function() {
+            var translate_x,
+                translate_y,
+                new_translate_y
+
+            for(var i = 0; i < 16; i++) {
+                translate_x = 0;
+                translate_y = 18 * i;
+
+                if(i > 8){
+                    new_translate_y = i;
+                    new_translate_y -= 7;
+
+                    translate_y = new_translate_y * 18;
+                    translate_x = 75;
+                }
+
+                d3.select(".nv-series:nth-child(" + i + ")")
+                    .attr("transform", "translate("+ translate_x + "," + translate_y + ")");
+            }
+        };
+});
