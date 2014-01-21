cfe5353d47acee4f64b348c5f7d24df361301597
crobays <maikelvlasman@me.com>
2014-01-21 04:41:36 +0100

    added jsonp callback to request

--- app/js/services.js
+++ app/js/services.js
@@ -10,8 +10,8 @@ angular.module('film_factor.services', []).
   	var maiVCApiService = {};
 
   	maiVCApiService.getMoviesData = function(subfactor) {
-      console.log('http://tff.maikelvlasman.com'+host+':8011/_api/movies/'+ subfactor +'/all/1000');
-  		return $http.jsonp('http://tff.maikelvlasman.com'+host+':8011/_api/movies/'+ subfactor +'/all/1000');
+      console.log('http://tff.maikelvlasman.com'+host+':8011/_api/movies/'+ subfactor +'/all/1000?_jsonp_callback=received_data');
+  		return $http.jsonp('http://tff.maikelvlasman.com'+host+':8011/_api/movies/'+ subfactor +'/all/1000?_jsonp_callback=received_data');
   	};
 
   	return maiVCApiService;
