ab2f3509af7177376ee9bb92492b450f6d10a0c3
crobays <maikelvlasman@me.com>
2014-01-21 15:39:03 +0100

    edit

--- app/js/services.js
+++ app/js/services.js
@@ -3,15 +3,15 @@
 /* Services */
 
 
-var host = window.location.host == 'local:8000' ? '.ld' : '';
+var host = window.location.host == 'http://tff.maikelvlasman.com.ld:8011' ? '' : 'http://tff.maikelvlasman.com:8011';
 
 angular.module('film_factor.services', []).
   factory('maiVCApiService', function($http) {
   	var maiVCApiService = {};
 
   	maiVCApiService.getMoviesData = function(subfactor) {
-      console.log('http://tff.maikelvlasman.com'+host+':8011/_api/movies/'+ subfactor +'/all/1000?_jsonp_callback=received_data');
-  		return $http.jsonp('http://tff.maikelvlasman.com'+host+':8011/_api/movies/'+ subfactor +'/all/1000?_jsonp_callback=received_data');
+      console.log(host+'/_api/movies/'+ subfactor +'/all/1000?_jsonp_callback=received_data');
+  		return $http.jsonp(host+'/_api/movies/'+ subfactor +'/all/1000?_jsonp_callback=received_data');
   	};
 
   	return maiVCApiService;
